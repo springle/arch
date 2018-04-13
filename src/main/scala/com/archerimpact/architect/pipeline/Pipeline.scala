53eb6a0754845bd6cec8483e0afd19d276a7989d
Sam Pringle <15springle@gmail.com>
2018-04-13 11:43:14 -0700

    Add PipelineSupervisor

--- src/main/scala/com/archerimpact/architect/pipeline/Pipeline.scala
+++ src/main/scala/com/archerimpact/architect/pipeline/Pipeline.scala
@@ -1,44 +1,24 @@
 package com.archerimpact.architect.pipeline
 
-import akka.actor.{ Actor, ActorLogging, ActorSystem, Props }
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import scala.io.StdIn
 
-object Printer {
-  def props: Props = Props[Printer]
-  final case class OutputMessage(msg: String)
+object PipelineSupervisor {
+  def props(): Props = Props(new PipelineSupervisor)
 }
 
-class Printer extends Actor with ActorLogging {
-  import Printer._
-
-  def receive = {
-    case "printit" =>
-      val secondRef = context.actorOf(Props.empty, "second-actor")
-      println(s"Second: $secondRef")
-    case OutputMessage(msg) =>
-      log.info(msg)
-  }
-}
-
-class StartStopActor1 extends Actor {
-  override def preStart(): Unit = {
-    println("first started")
-    context.actorOf(Props[StartStopActor2], "second")
-  }
-  override def postStop(): Unit = println("first stopped")
-  override def receive: Receive = {
-    case "stop" => context.stop(self)
-  }
-}
-
-class StartStopActor2 extends Actor {
-  override def preStart(): Unit = println("second started")
-  override def postStop(): Unit = println("second stopped")
-  override def receive: Receive = Actor.emptyBehavior
+class PipelineSupervisor extends Actor with ActorLogging {
+  override def preStart(): Unit = log.info("Architect Pipeline started")
+  override def postStop(): Unit = log.info("Architect Pipeline stopped")
+  override def receive = Actor.emptyBehavior
 }
 
 object Pipeline extends App {
   val system = ActorSystem("architectPipeline")
-  val printerActor = system.actorOf(Printer.props, "first-actor")
-  println(s"First: $printerActor")
-  printerActor ! "printit"
+  try {
+    system.actorOf(PipelineSupervisor.props(), "pipeline-supervisor")
+    StdIn.readLine()
+  } finally {
+    system.terminate()
+  }
 }
