71b6b60ed29d456602b46b0df5d2738acb5e6761
Tyler Heintz <tyler@Tylers-MacBook-Air.local>
2018-05-24 18:06:58 -0400

    Add HTMLParser

--- src/main/scala/com/archerimpact/architect/arch/parsers/formats/HTMLParser.scala
+++ src/main/scala/com/archerimpact/architect/arch/parsers/formats/HTMLParser.scala
@@ -2,17 +2,20 @@ package com.archerimpact.architect.arch.parsers.formats
 
 import com.archerimpact.architect.arch.parsers.Parser
 import com.archerimpact.architect.arch.shipments.GraphShipment
-import net.ruippeixotog.scalascraper.browser.JsoupBrowser
+import net.ruippeixotog.scalascraper.browser.{Browser, JsoupBrowser}
+import net.ruippeixotog.scalascraper._
 import org.json4s._
-import org.json4s.native.JsonMethods.{parse=>parseJson}
+import org.json4s.native.JsonMethods.{parse => parseJson}
 
 abstract class HTMLParser extends Parser {
 
-  def htmlToGraph(data: String, url: String): GraphShipment
+  def htmlToGraph(data: Browser#DocumentType, url: String): GraphShipment
 
   override def parse(data: Array[Byte], url: String): GraphShipment = {
     val htmlStr = data.map(_.toChar).mkString
-    htmlToGraph(htmlStr, url)
+    var browser = JsoupBrowser()
+    var doc = browser.parseString(htmlStr)
+    htmlToGraph(doc, url)
   }
 
 }
